b92dedbd0038acf5e89d5aae2cce0036b5734360
Thomas Lefevre <thomas.lefevre@coupa.com>
2018-10-23 16:20:32 +0200

    Implemented default values 2 for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,13 @@ class Portfolio < ApplicationRecord
   end
 
   scope :specific_portfolio_items, -> { where(title: 'Portfolio title: 7') }
+
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
+
 end
