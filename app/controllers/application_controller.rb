01d3e93c37bfb566408974f17065a98d23bdb3e5
Thomas Lefevre <thomas.lefevre@coupa.com>
2018-10-25 11:38:32 +0200

    Added session tracker. GET and Session ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
+  # #######################################################################
+  # THIS IS THE PARENT CONTROLLER OF ALL THE CONTROLLERS IN THE SYSTEM !! #
+  # #######################################################################
+
 
   #You can find it in devise_whitelist.rb
   include DeviseWhitelist
+
+  before_action :set_source
+
+  def set_source
+    session[:getVarInSession] = params[:q] if params[:q]
+
+
+  end
+
+
+
 
 end
