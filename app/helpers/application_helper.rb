ff0a5f4920183456667869f4e2d86dd6763b93bb
Thomas Lefevre <thomas.lefevre@coupa.com>
2018-10-30 09:46:19 +0100

    implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def login_helper
+
+    if current_user.is_a?(User) # if it is an "fake" Guest user it is type OpenStruct %>
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      # si on met pas les () avec le + ruby retourne juste Login car il retourne que le dernier.
+      (link_to "Sign up", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end
+
+  end
 end
