60fc920258d5c098810f0f66e2320d57527cf84f
Thomas Lefevre <thomas.lefevre@coupa.com>
2018-12-27 11:05:11 +0100

    Built out topic views

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,15 @@
+class TopicsController < ApplicationController
+  layout 'blog'
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    #@topic.blogs.
+
+    @blogs = logged_in?(:site_admin) ? @topic.blogs.recent.page(params[:page]).per(5) : @topic.blogs.published.recent.page(params[:page]).per(5)
+
+  end
+end
